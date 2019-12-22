a1995aa8a91befa4e4dfde16b52d70ce98382751
Edwin Vos <EE.Vos1@student.han.nl>
2019-12-22 22:49:54 +0100

    Added String Calculator

---
+++ stringcalculatorkata/src/main/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+
+    public static void main(String[] args) {
+        StringCalculator stringCalculator = new StringCalculator();
+        int result = stringCalculator.Add("123");
+        System.out.println(result);
+    }
+
+}
---
+++ stringcalculatorkata/src/main/java/StringCalculator.java
@@ -0,0 +1,6 @@
+public class StringCalculator {
+    public int Add(String numbers){
+        int result = Integer.parseInt(numbers);
+        return result;
+    }
+}
